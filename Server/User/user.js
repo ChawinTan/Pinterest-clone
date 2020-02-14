2b6469e50e8a6d4829fd5d8f92529b0eaf68e649
chawin <chawin15@gmail.com>
2020-02-14 10:45:59 +0800

    add facebook authentication

--- Server/User/user.js
+++ Server/User/user.js
@@ -1,6 +1,28 @@
 var express = require("express");
+var passport = require("passport")
+var facebookStrategy = require("passport-facebook").Strategy;
 var connection = require("../sql");
+var config = require("../config");
 var router = express.Router();
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+passport.use(new facebookStrategy({
+    clientID: config.facebookApiKey,
+    clientSecret: config.facebookApiSecret
+},
+    (accessToken, refreshToken, profile, done) => {
+        process.nextTick(() => {
+            console.log(profile);
+        })
+    }
+))
 
 router.get('/user', (req, res) => {
     let getUser = `SELECT * FROM User WHERE id = 1`;
@@ -11,6 +33,12 @@ router.get('/user', (req, res) => {
             res.status(200).json(data);
         }
     });
+});
+
+router.get('/facebook/login', passport.authenticate('facebook', {scope: 'email'}));
+
+router.get('/callback', (req, res) => {
+    res.status(200).json({login: 'success'});
 });
 
 module.exports = router;
