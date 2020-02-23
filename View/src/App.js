1e6dca0346601b94473ae23dac8f95afc7bd580e
chawin <chawin15@gmail.com>
2020-02-23 19:51:11 +0800

    get user when load

--- View/src/App.js
+++ View/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Navbar from "./Components/Navbar/navbar";
 
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const url = 'https://localhost:8081/user/user';
+    
+    fetch(url, {
+      method: 'get',
+      headers: { 
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .then(json => console.log(json));
+  })
+
   return (
     <div className="App">
       <Navbar />
