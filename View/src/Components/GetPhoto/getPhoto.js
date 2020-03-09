2aba2247bd5f0a437fc2e763204b27b483829f82
chawin <chawin15@gmail.com>
2020-03-09 15:48:22 +0800

    remove get facebook photo api

--- View/src/Components/GetPhoto/getPhoto.js
+++ View/src/Components/GetPhoto/getPhoto.js
@@ -28,36 +28,16 @@ const useStyles = makeStyles(theme  => ({
     }
 }))
 
-const fetchPhoto = (userId) => {
-    fetch('https://localhost:8081/user/tokens', {
-        method: 'get',
-        headers: { 
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-          }
-    }).then(res => res.json())
-    .then(json => json.accessToken)
-    .then(token => {
-        fetch(`https://graph.facebook.com/${userId}/photos?access_token=${token}`, {
-            method: 'get',
-            headers: { 
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-              }
-        }).then(res => res.json())
-        .then(json => console.log(json))
-    })
-}
 
 function GetPhoto(props) {
     const classes = useStyles();
-    const { getPhoto, userId } =  props;
+    const { userId } =  props;
 
     return (
         <div style={styles.root}>
             <Paper className={classes.paper}>
-                <Button className={classes.button} onClick={() => {fetchPhoto(userId)}}>{ getPhoto === 0 ? 'Get Photos' : 'Update Photos' }</Button>
-                <span style={styles.span}>{ getPhoto === 0 ? 'Click to import photos from your facebook' : 'Click to import new photos'}</span>
+                <Button className={classes.button} >Add Photo</Button>
+                <span style={styles.span}>Click to add photos</span>
             </Paper>
         </div>
     )
