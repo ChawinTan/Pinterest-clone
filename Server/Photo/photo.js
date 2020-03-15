945d55c2d80814b0ecb00f6f8f85acaca263abec
chawin <chawin15@gmail.com>
2020-03-15 11:55:43 +0800

    get photo endpoint

--- Server/Photo/photo.js
+++ Server/Photo/photo.js
@@ -2,6 +2,18 @@ var express = require("express");
 var connection = require("../sql");
 var router = express.Router();
 
+router.get('/get/:user_id', (req, res) => {
+    const get_photo = `SELECT * FROM Photo WHERE user_id = ?`;
+
+    connection.query(get_photo, req.params.user_id, (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            res.status(200).json(data);
+        }
+    });
+})
+
 router.post('/add/:user_id', (req, res) => {
     const add_photo = `INSERT INTO Photo (user_id, link) VALUES (? ,?)`;
 
@@ -24,6 +36,6 @@ router.get('/delete/:id', (req, res) => {
             res.status(200).json({status: 'deleted'});
         }
     });
-})
+});
 
 module.exports = router;
