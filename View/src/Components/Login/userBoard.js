06aa481ec10ffa677737f479a68e848774a41d38
chawin <chawin15@gmail.com>
2020-03-21 20:27:53 +0800

    display pictures for userboard

--- View/src/Components/Login/userBoard.js
+++ View/src/Components/Login/userBoard.js
@@ -1,26 +1,51 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+
+const styles = {
+    root: {
+        padding: '1rem',
+    }
+}
+
+const useStyle = makeStyles(() => ({
+    cardMedia: {
+        height: '250px'
+    }
+}));
 
 function UserBoard(props)  {
-    const { userId } = props;
-    
-    // useEffect(() => {
-    //     const url = `https://localhost:8081/photo/get/${userId}`;
-
-    //     fetch(url, {
-    //         method: 'get',
-    //         headers: { 
-    //             Accept: 'application/json',
-    //             'Content-Type': 'application/json'
-    //         }
-    //     }).then(res => res.json())
-    //     .then(json => { 
-    //         storePhotos(json);
-    //     })
-    // })
+    const classes = useStyle();
+    const { userId, photos } = props;
 
     return (
-        <div>Userboard works!</div>
+        <div style={styles.root}>
+            <Grid container spacing={3}>
+                {
+                    photos.map((photo, id) => {
+                        console.log(photo.link)
+                        return (
+                            <Grid key={id} item xs={3}>
+                                <Card>
+                                    <CardMedia
+                                    className={classes.cardMedia}
+                                    title='User picture'
+                                    image={photo.link}
+                                    component="img"
+                                     />
+                                    <CardContent>
+                                        Coming soon
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
+        </div>
     )
 }
 
