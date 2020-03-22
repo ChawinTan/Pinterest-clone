f3c5bf6aaba136cbb8f4f7b4061557a6c3e8c82b
chawin <chawin15@gmail.com>
2020-03-22 14:25:06 +0800

    add card content

--- View/src/Components/Login/userBoard.js
+++ View/src/Components/Login/userBoard.js
@@ -4,16 +4,30 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import IconButton from '@material-ui/core/IconButton';
 
 const styles = {
     root: {
         padding: '1rem',
+    },
+    description: {
+        fontFamily: "Comic Sans MS",
+        fontWeight: 600,
+        color: '#808080'
+    },
+    input: {
+        padding: '0.2rem',
+        width: '200px'
     }
 }
 
 const useStyle = makeStyles(() => ({
     cardMedia: {
         height: '250px'
+    },
+    deleteIcon: {
+        color: '#ff4d4d',
     }
 }));
 
@@ -36,7 +50,17 @@ function UserBoard(props)  {
                                     component="img"
                                      />
                                     <CardContent>
-                                        Coming soon
+                                        { 
+                                            userId === photo.user_id && photo.description ?
+                                            <span style={styles.description}>{photo.description}</span>
+                                            :
+                                            userId === photo.user_id && !photo.description ?
+                                            <input style={styles.input} placeholder="Enter short description" />
+                                            :
+                                            null
+
+                                        }
+                                        { userId === photo.user_id ? <IconButton className={classes.deleteIcon} ><DeleteForeverIcon/></IconButton> : null }
                                     </CardContent>
                                 </Card>
                             </Grid>
