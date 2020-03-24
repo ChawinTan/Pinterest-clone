1e275edf8cfb1005e9c6d0bb323eec97c079706d
chawin <chawin15@gmail.com>
2020-03-24 17:45:37 +0800

    update description

--- View/src/Components/Login/userBoard.js
+++ View/src/Components/Login/userBoard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useImperativeHandle } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -6,6 +6,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import IconButton from '@material-ui/core/IconButton';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 
 const styles = {
     root: {
@@ -18,7 +19,7 @@ const styles = {
     },
     input: {
         padding: '0.2rem',
-        width: '200px'
+        width: '150px'
     }
 }
 
@@ -28,12 +29,32 @@ const useStyle = makeStyles(() => ({
     },
     deleteIcon: {
         color: '#ff4d4d',
+    },
+    checkIcon: {
+        color: '#00b33c',
+        marginLeft: '1rem'
     }
 }));
 
 function UserBoard(props)  {
     const classes = useStyle();
     const { userId, photos } = props;
+
+    const [photoCopy, setPhotoCopy] = useState([]);
+    const handleDescChange = (e, index) => {
+        const updatePhoto = photoCopy.length === 0 ? [...photos] : [...photoCopy];
+
+        for (let i=0; i < photos.length; i++) {
+            if (i === index) {
+                updatePhoto[i] = { ... updatePhoto[i], description: e.target.value };
+            }
+        }
+        setPhotoCopy(updatePhoto);
+    }
+
+    const handleDescClick = (index) => {
+        console.log(photoCopy);
+    }
 
     return (
         <div style={styles.root}>
@@ -55,11 +76,16 @@ function UserBoard(props)  {
                                             <span style={styles.description}>{photo.description}</span>
                                             :
                                             userId === photo.user_id && !photo.description ?
-                                            <input style={styles.input} placeholder="Enter short description" />
+                                            <input 
+                                            name={`desc-${ id }`} 
+                                            style={styles.input} 
+                                            onChange={(e) => {handleDescChange(e, id)}}
+                                            placeholder="Enter short description" />
                                             :
                                             null
 
                                         }
+                                        { userId === photo.user_id ? <IconButton className={classes.checkIcon} onClick={handleDescClick}><CheckCircleIcon/></IconButton> : null }
                                         { userId === photo.user_id ? <IconButton className={classes.deleteIcon} ><DeleteForeverIcon/></IconButton> : null }
                                     </CardContent>
                                 </Card>
